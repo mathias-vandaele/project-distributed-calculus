3927faf4f4de0e6cd8ac34939756e66d29169150
Mathias Vandaele <mathias.vandaele@camunda.com>
2026-04-28 11:54:29 +0800

    fix ws

--- frontend/src/endpoints.rs
+++ frontend/src/endpoints.rs
@@ -2,12 +2,24 @@ use wasm_bindgen::JsCast;
 
 pub fn websocket_url() -> String {
     let scope = js_sys::global().unchecked_into::<web_sys::WorkerGlobalScope>();
-    let location = scope.location();
-    let scheme = if location.protocol() == "https:" {
-        "wss"
-    } else {
-        "ws"
-    };
+    if let Some(url) = websocket_url_from_origin(&scope.origin()) {
+        return url;
+    }
 
-    format!("{}://{}/ws", scheme, location.host())
+    let location = scope.location();
+    websocket_url_from_origin(&location.origin())
+        .or_else(|| websocket_url_from_origin(&location.href()))
+        .expect("worker origin must be http or https")
+}
+
+fn websocket_url_from_origin(origin: &str) -> Option<String> {
+    let origin = origin.strip_prefix("blob:").unwrap_or(origin);
+
+    if let Some(host) = origin.strip_prefix("https://") {
+        Some(format!("wss://{}/ws", host.split('/').next().unwrap_or(host)))
+    } else if let Some(host) = origin.strip_prefix("http://") {
+        Some(format!("ws://{}/ws", host.split('/').next().unwrap_or(host)))
+    } else {
+        None
+    }
 }
